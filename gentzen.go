9e34692982c98a0e9d7d3ef147909aa5d9e6444c
Pranay <66739544+PranayB003@users.noreply.github.com>
2023-10-05 02:21:07 +0530

    reduced: proof tree fanout

--- gentzen.go
+++ gentzen.go
@@ -54,48 +54,47 @@ func (exp Expression) Proove() bool {
 
 // decompose a sequent by using one of the 8 rules
 func (seq Sequent) Decompose() Sequents {
-  res := make(Sequents, 0)
   ant, con := seq.ant, seq.con
-
-  // left rules
-  for i := 0; i < len(ant); i++ {
-    exp := ant[i]
-    newAnt := ant[:i].Append(ant[i+1:]...)
-    if exp.etype == 2 && exp.mid == _not {
-      res = append(res, Sequent{newAnt, con.Append(*(exp.right))})
-    } else if exp.etype == 3 && exp.mid == _and {
-      res = append(res, Sequent{newAnt.Append(*(exp.left), *(exp.right)), con})
-    } else if exp.etype == 3 && exp.mid == _or {
-      newAnt1 := newAnt.Append(*(exp.left))
-      newAnt2 := newAnt.Append(*(exp.right))
-      res = append(res, Sequent{newAnt1, con}, Sequent{newAnt2, con})
-    } else if exp.etype == 3 && exp.mid == _imp {
-      newAnt1 := newAnt.Append(*(exp.right))
-      newCon2 := con.Append(*(exp.left))
-      res = append(res, Sequent{newAnt, newCon2}, Sequent{newAnt1, con})
-    }
-  }
 
   // right rules
   for i := 0; i < len(con); i++ {
     exp := con[i]
     newCon := con[:i].Append(con[i+1:]...)
     if exp.etype == 2 && exp.mid == _not {
-      res = append(res, Sequent{ant.Append(*(exp.right)), newCon})
+      return Sequents{Sequent{ant.Append(*(exp.right)), newCon}}
     } else if exp.etype == 3 && exp.mid == _or {
-      res = append(res, Sequent{ant, newCon.Append(*(exp.left), *(exp.right))})
-    } else if exp.etype == 3 && exp.mid == _and {
-      newCon1 := newCon.Append(*(exp.left))
-      newCon2 := newCon.Append(*(exp.right))
-      res = append(res, Sequent{ant, newCon1}, Sequent{ant, newCon2})
+      return Sequents{Sequent{ant, newCon.Append(*(exp.left), *(exp.right))}}
     } else if exp.etype == 3 && exp.mid == _imp {
       newCon1 := newCon.Append(*(exp.right))
       newAnt := ant.Append(*(exp.left))
-      res = append(res, Sequent{newAnt, newCon1})
-    }
+      return Sequents{Sequent{newAnt, newCon1}}
+    } else if exp.etype == 3 && exp.mid == _and {
+      newCon1 := newCon.Append(*(exp.left))
+      newCon2 := newCon.Append(*(exp.right))
+      return Sequents{Sequent{ant, newCon1}, Sequent{ant, newCon2}}
+    }  
   }
 
-  return res
+  // left rules
+  for i := 0; i < len(ant); i++ {
+    exp := ant[i]
+    newAnt := ant[:i].Append(ant[i+1:]...)
+    if exp.etype == 2 && exp.mid == _not {
+      return Sequents{Sequent{newAnt, con.Append(*(exp.right))}}
+    } else if exp.etype == 3 && exp.mid == _and {
+      return Sequents{Sequent{newAnt.Append(*(exp.left), *(exp.right)), con}}
+    } else if exp.etype == 3 && exp.mid == _or {
+      newAnt1 := newAnt.Append(*(exp.left))
+      newAnt2 := newAnt.Append(*(exp.right))
+      return Sequents{Sequent{newAnt1, con}, Sequent{newAnt2, con}}
+    } else if exp.etype == 3 && exp.mid == _imp {
+      newAnt1 := newAnt.Append(*(exp.right))
+      newCon2 := con.Append(*(exp.left))
+      return Sequents{Sequent{newAnt, newCon2}, Sequent{newAnt1, con}}
+    }
+  }
+  
+  return Sequents{}
 }
 
 // returns false if the sequent has a contradiction
